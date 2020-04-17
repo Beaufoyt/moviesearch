2a47ca3f56e0450c1a379e7522d36c4054ed5a62
beaufoyt <beaufoyyy@live.com>
2020-04-17 11:49:23 +0100

    Adds movie DB search action logic

--- src/actions/movies.js
+++ src/actions/movies.js
@@ -1,21 +1,47 @@
+import axios from 'axios';
 import types from '../constants/actionTypes';
 
 import { getErrorMessage } from '../helpers/responses';
+import { build } from '../helpers/urlQuery';
+
+const { CancelToken } = axios;
+let cancelMovies;
 
 export const moviesIsLoading = () => ({ type: types.MOVIES_IS_LOADING });
 export const moviesSuccess = (movies) => ({ type: types.MOVIES_SUCCESS, movies });
 export const moviesError = (errorMessage) => ({ type: types.MOVIES_ERROR, errorMessage });
+export const moviesCancel = () => ({ type: types.MOVIES_CANCEL });
 
-export const fetchMovies = () => {
+export const fetchMovies = (query, page) => {
     return async (dispatch) => {
         try {
             dispatch(moviesIsLoading());
 
-            const data = [];
+            const { data } = await axios.get(build('https://api.themoviedb.org/3/search/movie', {
+                api_key: '95107023211442633a2ddddd9a7259ee',
+                language: 'en-US',
+                query,
+                page,
+            }), {
+                cancelToken: new CancelToken((c) => {
+                    cancelMovies = c;
+                }),
+            });
 
-            dispatch(moviesSuccess(data));
+            dispatch(moviesSuccess(data, query, page));
         } catch (err) {
-            dispatch(moviesError(getErrorMessage(err)));
+            if (axios.isCancel(err)) {
+                console.log(err.message);
+            } else {
+                dispatch(moviesError(getErrorMessage(err)));
+            }
         }
+    };
+};
+
+export const cancelFetchMovies = () => {
+    return async (dispatch) => {
+        cancelMovies('Movie request cancelled by route change');
+        dispatch(moviesCancel());
     };
 };
