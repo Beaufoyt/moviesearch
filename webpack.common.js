3d939fe8de2b5ddc573ed7e3a7dc57d9de9554d3
beaufoyt <beaufoyyy@live.com>
2020-04-17 10:35:04 +0100

    Adds webpack config

---
+++ webpack.common.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+        'babel-polyfill',
+        './src/index.jsx',
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: ['babel-loader', {
+                    loader: 'eslint-loader',
+                    options: {
+                        emitWarning: true,
+                    },
+                }],
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader',
+            },
+            {
+                test: /\.(gif|svg|jpg|png)$/,
+                loader: 'file-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/',
+                    },
+                }],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx'],
+    },
+    plugins: [
+        new webpack.EnvironmentPlugin({
+            NODE_ENV: 'development',
+            MOVIEDB_API_KEY: '95107023211442633a2ddddd9a7259ee',
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+            favicon: 'src/images/favicon.png',
+            inject: false,
+        }),
+    ],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'bundle.js',
+    },
+};
